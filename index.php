b4128c537bc42e0d06ca779ca9637746b6f0780a
A̶k̶s̶h̶a̶z̶u̶r̶ ̶|̶ ̶d̶e̶v̶.̶ <92050792+Akshazz@users.noreply.github.com>
2024-08-19 14:19:16 +0800

    prep

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <head>
+
+            <meta lang="en">
+            <title> PSYC WEB </title>
+            <link rel="stylesheet" href="/css/style.css">
+
+        </head>
+<p>  <p>
+</html>
+</body>
